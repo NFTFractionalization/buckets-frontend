96effd00ec5680224bde4a21fe11f7924f755132
green <ewitulsk@calpoly.edu>
2022-02-06 04:40:57 -0800

    can fetch deposits, shows user accout

---
+++ src/VaultInfo.js
@@ -0,0 +1,796 @@
+exports.VaultAddr = "0x156FeCab42F950D87E2888176d21946350e69D82"
+exports.VaultABI = [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "nftAddr",
+				"type": "address"
+			}
+		],
+		"name": "Recieved",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "nftAddr",
+				"type": "address"
+			}
+		],
+		"name": "Released",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "xChainInternalId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "nftAddr",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "chainId",
+				"type": "uint256"
+			}
+		],
+		"name": "XChainRegistered",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "xChainInternalId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "nftAddr",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "chainId",
+				"type": "uint256"
+			}
+		],
+		"name": "XChainRelease",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "approveNFTokenTransfer",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "buyoutERC721",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_internalId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_FracToBuy",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_buyer",
+				"type": "address"
+			}
+		],
+		"name": "buyTokens",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "FracToBuy",
+				"type": "uint256"
+			}
+		],
+		"name": "buyTokensIndividual",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "depositNFToken",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "supply",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "ticker",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amountToKeep",
+				"type": "uint256"
+			}
+		],
+		"name": "mintTokensForNFT",
+		"outputs": [
+			{
+				"internalType": "contract NFToken",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "operator",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bytes",
+				"name": "data",
+				"type": "bytes"
+			}
+		],
+		"name": "onERC721Received",
+		"outputs": [
+			{
+				"internalType": "bytes4",
+				"name": "",
+				"type": "bytes4"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "xChainInternalId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "nftAddr",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "chainId",
+				"type": "uint256"
+			}
+		],
+		"name": "registerXChainNFT",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_internalId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_FracToSell",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_seller",
+				"type": "address"
+			}
+		],
+		"name": "sellTokens",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amountOfwEth",
+				"type": "uint256"
+			}
+		],
+		"name": "sellTokensIndividual",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_wEthAddr",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "oracle",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amountOfwEth",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_internalId",
+				"type": "uint256"
+			}
+		],
+		"name": "calculateAmountOfFrac",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amountOfFrac",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_internalId",
+				"type": "uint256"
+			}
+		],
+		"name": "calculateAmountOfwEth",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "getDepositAmount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			}
+		],
+		"name": "getERC721ChainId",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			}
+		],
+		"name": "getERC721ContractAddr",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			}
+		],
+		"name": "getERC721Native",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			}
+		],
+		"name": "getERC721TokenId",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getInternalIdCounter",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			}
+		],
+		"name": "getNFTContractAddr",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			}
+		],
+		"name": "getNFTokenAddr",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "getNFTokenBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			}
+		],
+		"name": "getNFTokenSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getNumberDepositedERC721s",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "getNumIdsOwned",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "internalId",
+				"type": "uint256"
+			}
+		],
+		"name": "getOwned",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "getOwnedInternalIds",
+		"outputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getwEthAddr",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
--- src/pages/Liquifiy.js
+++ src/pages/Liquifiy.js
@@ -7,17 +7,44 @@ import userData from '../data/userWallet';
 import SetupTokens from '../components/SetupTokens.js'
 
 // load json from userWallet.json into object
-import { useMoralis, authenticate, enableWeb3 } from "react-moralis";
+import { useMoralis, authenticate, enableWeb3, useWeb3ExecuteFunction } from "react-moralis";
 // import {Token } from "react-moralis";
 import { Units} from "moralis";
 import { useWeb3Transfer } from 'react-moralis';
 import { useEffect } from 'react';
+import { VaultAddr, VaultABI } from '../VaultInfo';
+
+function ErrorMessage(props){
+    return(
+        <div>{props.error}</div>
+    )
+}
+
+function GetDeposits(props){
+    const {data, error, fetch, isFetching, isLoading} = useWeb3ExecuteFunction();
+    let options = {
+        abi: VaultABI,
+        contractAddress: VaultAddr,
+        functionName: "getOwnedInternalIds",
+        params: {
+            account: props.userAddr
+        }
+    }
+
+    return (<div>
+        {error && <ErrorMessage error={error} />}
+        <button onClick={() => fetch({ params: options })} disabled={isFetching}>Fetch data</button>
+        {data && <pre>
+          {JSON.stringify(data)}
+        </pre>}
+      </div>)
+}
 
 function TransferNFT(props){
     const {fetch, error, isFetching} = useWeb3Transfer({
         type: "erc721",
         amount: Units.Token(1,18),
-        receiver: "0x54D713fDbDC1bD9634A83cF1ebe3Cb91825d536f", /* Daniel's address */
+        receiver: VaultAddr, /* Daniel's address */
         contractAddress: props.nft.token_address,
         tokenId: props.nft.token_id,
         // params: {chainId:"mumbui"}
@@ -38,21 +65,24 @@ function TransferNFT(props){
 
 
 function Liquifiy(props) {
-    const {isInitialized, Moralis, authenticate, isAuthenticated} = useMoralis();
+    const {isInitialized, Moralis, authenticate, isAuthenticated, user} = useMoralis();
     const [userNFTs, setUserNFTs] = useState([]);
+    const [userAddr, setUserAddr] = useState("");
+    const [currentChain, setCurrentChain] = useState("mumbai");
     const [selectedNFT, setSelectedNFT] = useState({});
-    // sending a token with token id = 1
-    
+    // sending a token with token id = 1    
     
     useEffect(()=>{
-        
         const EWeb3 = async () =>{
             await authenticate();
             if(isInitialized, isAuthenticated){
-                Moralis.Web3.getNFTs({chain: "mumbai"}).then(setUserNFTs);
+                Moralis.Web3.getNFTs({chain: currentChain}).then(setUserNFTs);
+                setUserAddr(user.get("ethAddress"));
             }
         }
         EWeb3();
+
+        
         
     }, [isInitialized, Moralis]);
     
@@ -65,11 +95,11 @@ function Liquifiy(props) {
             <div className=" p-3 ">
                 <div className=" text-white text-center ">
                     <h1 className="text-3xl m-3"> Liquifiy your nft </h1>
+                    <h1 className="text-2xl m-3"> {userAddr} </h1>
                     <div className="grid grid-cols-3 gap-3">
                         {/* Deposit nft section */}
                         <div className="bg-white opacity-80 rounded-2xl p-4 m-2 overflow-y-auto h-96">
                             <p className=" opacity-100 text-transparent text-3xl bg-clip-text bg-gradient-to-br from-[#1b82f8] to-[#a70b78]"> Your NFTs</p>
-                    
                             {userNFTs.map(nft => {
                                 return (
                                     <div className="text-left text-gray-500 grid grid-cols-3 p-1">
@@ -87,13 +117,8 @@ function Liquifiy(props) {
                             })}
                         </div>
                         <div className="rounded-3xl  bg-gradient-to-tl from-rose-400 to-orange-300 mt-19">
-                            <h1 className="text-xl">Deposit your NFT</h1>
-                            {/* <NFTBalances /> */}
-                            <p className="text-white text-2xl">Selected NFT</p>
-                            
-                            {!selectedNFT ? <p> No NFT selected</p> : <p> {selectedNFT.name}</p>}
-                            {/* <OutlinedButton  text="Deposit NFT" onClick={(options)=>transfer_nft(options)} /> */}
-
+                            <h1 className="text-xl">Your Deposits</h1>
+                            <GetDeposits userAddr={userAddr}/>
                         </div>
                         {/* Configure liq */}
                         <SetupTokens />
