6dacca7c726b031f2a8efc093f10b10fb6faca44
green <ewitulsk@calpoly.edu>
2022-02-06 02:10:55 -0800

    aggressive authing, scrollable nfts, transfer button

--- src/pages/Liquifiy.js
+++ src/pages/Liquifiy.js
@@ -10,57 +10,54 @@ import SetupTokens from '../components/SetupTokens.js'
 import { useMoralis, authenticate, enableWeb3 } from "react-moralis";
 // import {Token } from "react-moralis";
 import { Units} from "moralis";
-import { useNFTBalances } from "react-moralis";
 import { useWeb3Transfer } from 'react-moralis';
 import { useEffect } from 'react';
 
-const TransferNFT = (nft) => {
-    // const { authenticate, isAuthenticated, user } = useMoralis();
-    
+function TransferNFT(props){
     const {fetch, error, isFetching} = useWeb3Transfer({
         type: "erc721",
         amount: Units.Token(1,18),
         receiver: "0x54D713fDbDC1bD9634A83cF1ebe3Cb91825d536f", /* Daniel's address */
-        contractAddress: "0x519498450961b093D78ab616A59fDadA2bc5C1f5",
-        tokenId: 1,
+        contractAddress: props.nft.token_address,
+        tokenId: props.nft.token_id,
         // params: {chainId:"mumbui"}
+
+        //THIS ISNT ACTUALLY RUNNING ;(
+        onSuccess: ()=>{
+            console.log("TRANSFER SUCCEEDED!!!");
+            //props.userNFTs.filter(function(nft){return nft === props.nft});
+            //props.setUserNFTs(props.userNFTs);
+        }
     });
   
     return (<div>
-        {/* {isAuthenticated?<div>{user.getUser()}</div>:"NONE"} */}
       {error && <div>{error.toString()} </div>}
-      <button onClick={() => fetch()} disabled={isFetching}>Transfer</button>
+      <button className="bg-black rounded-2xl text-white m-3" onClick={() => fetch()} disabled={isFetching}>Transfer</button>
     </div>)
   }
 
 
 function Liquifiy(props) {
-    const {isInitialized, Moralis} = useMoralis();
+    const {isInitialized, Moralis, authenticate, isAuthenticated} = useMoralis();
     const [userNFTs, setUserNFTs] = useState([]);
     const [selectedNFT, setSelectedNFT] = useState({});
     // sending a token with token id = 1
     
     
     useEffect(()=>{
-        if(isInitialized){
-            Moralis.Web3.getNFTs({chain: "mumbai"}).then(setUserNFTs);
+        
+        const EWeb3 = async () =>{
+            await authenticate();
+            if(isInitialized, isAuthenticated){
+                Moralis.Web3.getNFTs({chain: "mumbai"}).then(setUserNFTs);
+            }
         }
+        EWeb3();
+        
     }, [isInitialized, Moralis]);
     
     if(!isInitialized){
         return null;
-    }
-
-    /*
-    Moralis.onChainChanged(async function (chain){
-        console.log(chain);
-    });*/
-
-    const options = {
-        type: "erc721",
-        receiver: "0x54D713fDbDC1bD9634A83cF1ebe3Cb91825d536f", /* Daniel's address */
-        contractAddress: "0x2953399124f0cbb46d2cbacd8a89cf0599974963",
-        tokenId: "0xf163ad3c908d158924f0ed0f6ea26ee76951edef000000000000010000000001"
     }
    
     return (
@@ -70,7 +67,7 @@ function Liquifiy(props) {
                     <h1 className="text-3xl m-3"> Liquifiy your nft </h1>
                     <div className="grid grid-cols-3 gap-3">
                         {/* Deposit nft section */}
-                        <div className="bg-white opacity-80 rounded-2xl p-4 m-2 ">
+                        <div className="bg-white opacity-80 rounded-2xl p-4 m-2 overflow-y-auto h-96">
                             <p className=" opacity-100 text-transparent text-3xl bg-clip-text bg-gradient-to-br from-[#1b82f8] to-[#a70b78]"> Your NFTs</p>
                     
                             {userNFTs.map(nft => {
@@ -85,10 +82,7 @@ function Liquifiy(props) {
                                             {nft.name}
                                             <a href={nft.token_address}><p className="text-cyan-400 text-ellipsis overflow-hidden">{nft.token_address}</p></a>
                                         </div>
-                                        <button value={nft} onClick={(e) => {
-                                            setSelectedNFT(e.target.value)
-                                            }} className="bg-black rounded-2xl text-white m-3">select</button>
-
+                                        <TransferNFT nft={nft} userNFTs={userNFTs} setUserNFTs={setUserNFTs}/>
                                     </div>);
                             })}
                         </div>
@@ -96,8 +90,8 @@ function Liquifiy(props) {
                             <h1 className="text-xl">Deposit your NFT</h1>
                             {/* <NFTBalances /> */}
                             <p className="text-white text-2xl">Selected NFT</p>
-                            <TransferNFT/>
-                            {!selectedNFT ? <p> No NFT selected</p> : <p> {selectedNFT.toString()}</p>}
+                            
+                            {!selectedNFT ? <p> No NFT selected</p> : <p> {selectedNFT.name}</p>}
                             {/* <OutlinedButton  text="Deposit NFT" onClick={(options)=>transfer_nft(options)} /> */}
 
                         </div>
