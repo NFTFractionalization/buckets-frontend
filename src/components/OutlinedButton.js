d08d682f9f0cb2461b1c202f08dbfebd7156846f
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-02-11 16:47:16 -0800

    added hover to outlined button

--- src/components/OutlinedButton.js
+++ src/components/OutlinedButton.js
@@ -3,7 +3,7 @@ function OutlinedButton(props) {
     return (
         <div className="pt-4">
             <button onClick={() => props.onClick()} className="rounded-3xl bg-gradient-to-r p-[3px] from-[#7928ca] to-[#ff0080] hover:bg-gradient-to-r p-[3px] from-[#7928ca] to-[#ff0080]">
-                <div className="flex justify-between bg-black rounded-3xl">
+                <div className="flex justify-between bg-black rounded-3xl hover:bg-gradient-to-r p-[3px] from-[#7928ca] to-[#ff0080]">
                     <div className="text-white decoration-4 text-light flex m-2 pl-3 pr-3">
                         {(props !== undefined && props.text !== undefined) ? props.text : "Browse Buckets"}
                     </div>
